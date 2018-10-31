10860003e1693e2374042bbbab5c5c29e9460f73
klisman <popovovo12>
2018-10-30 21:29:59 -0300

    Ajustes de layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './views/home/home.component';
 import {ProjetosComponent} from './views/projetos/projetos.component';
 import {CertificadosComponent} from './views/certificados/certificados.component';
-import {ContatoComponent} from "./views/contato/contato.component";
+import {ContatoComponent} from './views/contato/contato.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
